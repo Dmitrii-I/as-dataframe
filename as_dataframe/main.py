00bf286e30217d91a6fd46561ae5a1bb83fb3c42
Dmitrii Izgurskii <izgurskii@gmail.com>
2018-03-12 00:11:30 +0100

    Save work in progress

--- as_dataframe/main.py
+++ as_dataframe/main.py
@@ -1,8 +1,7 @@
 
 from collections import defaultdict
 from pandas import DataFrame
-from copy import deepcopy
-from typing import Dict, List, NewType
+from typing import Dict, List, NewType, Any
 
 
 TypeDataFrame = NewType('TypeDataFrame', DataFrame)
@@ -20,13 +19,13 @@ def as_dataframe(obj: Dict or List[Dict]) -> TypeDataFrame:
 
     combined_df = DataFrame()
     for d in obj:
-        flat = _flattened(d)
+        flat = _flattened_2(d)
         num_rows = max([1] + [len(flat[k]) for k in flat if isinstance(flat[k], list)])
         df = DataFrame(flat, index=range(num_rows))
         combined_df = combined_df.append(df, ignore_index=False, verify_integrity=False)
 
     combined_df = combined_df.reset_index(drop=True)
-    combined_df = combined_df.dropna(axis='columns', how='all')
+    #combined_df = combined_df.dropna(axis='columns', how='all')
     return combined_df
 
 
@@ -46,38 +45,107 @@ def _flattened(nested_dict: dict) -> dict:
             v = v[0]
 
         if isinstance(v, dict):
-            new = _flattened(v)
-            flat.update({str(k) + '.' + kk: new[kk] for kk in new})
+            flattened = _flattened(v)
+            flattened = {str(k) + '.' + k_flattened: flattened[k_flattened] for k_flattened in flattened}
+            flat.update(flattened)
         elif isinstance(v, list):
             if all([isinstance(el, dict) for el in v]):
-                new = _single_dict(v)
+                new = _gathered(v)
                 flat.update({str(k) + '.' + kk: new[kk] for kk in new})
             else:
                 flat.update({k: v})
         else:
             flat.update({k: v})
+
     return flat
 
 
-def _single_dict(dicts: list, missing_value: str=None) -> dict:
+def _flattened_2(dictionary: dict) -> dict:
+
+    tree = TreeNode()
+
+    for k, v in dictionary.items():
+
+        node = TreeNode(name=k)
+
+        if isinstance(v, list) and len(v) == 1:
+            v = v[0]
+
+        if isinstance(v, dict):
+            v = _flattened_2(v)
+            for k_flattened, v_flattened in v.items():
+                sub_node = TreeNode(name=k_flattened, data=v_flattened)
+                node.add_child(sub_node)
+        elif isinstance(v, list):
+            if all([isinstance(el, dict) for el in v]):
+                v = [_flattened_2(d) for d in v]  ## replace by as_tree
+                v = _gathered(v)
+                for k_flattened, v_flattened in v.items():
+                    sub_node = TreeNode(name=k_flattened, data=v_flattened)
+                    node.add_child(sub_node)
+            else:
+                node.data = v
+        else:
+            node.data = v
+
+        tree.add_child(node)
+
+    flattened = {leaf.full_name: leaf.data for leaf in tree.leaf_nodes()}
+
+    return flattened
+
+
+def _as_tree(d: dict):
+    pass
+
+def _gathered(dictionaries: list, missing_value: str=None) -> dict:
     """ Returns a single dictionary built from a list of dictionaries
 
-    :param dicts: List of dictionaries. The dictionaries are allowed to be heterogeneous.
+    :param dictionaries: List of non-nested dictionaries. The dictionaries are allowed to be heterogeneous.
     :param missing_value: How to represent missing values when the dictionaries are heterogeneous.
     :returns: A single dictionary.
     """
 
-    dicts = deepcopy(dicts)
+    all_keys = set([key for d in dictionaries for key in list(d.keys())])
+    dictionary = dict(zip(all_keys, [list() for _ in range(len(all_keys))]))
 
-    for i in range(len(dicts)):
-        dicts[i] = _flattened(dicts[i])
-
-    keys = set([key for d in dicts for key in list(d.keys())])
-    dictionary = dict(zip(keys, [list() for i in range(len(keys))]))
-
-    for d in dicts:
-        new = defaultdict(lambda: missing_value, **d)
-        for k in keys:
-            dictionary[k].append(new[k])
+    for d in dictionaries:
+        d = defaultdict(lambda: missing_value, **d)
+        for k in all_keys:
+            dictionary[k].append(d[k])
 
     return dictionary
+
+
+class TreeNode:
+    def __init__(self, name: str=None, data: Any=None):
+        self.name = name
+        self.data = data
+        self.children = []
+        self.parents = []
+        self.delimiter = '.'
+
+    def add_child(self, node):
+
+        if self.data:
+            raise ValueError('Cannot add children because the node contains data')
+
+        node.parents = self.parents + [self]
+        self.children.append(node)
+
+    def leaf_nodes(self):
+        leaves = []
+
+        for node in self.children:
+            if not node.children:
+                leaves.append(node)
+            else:
+                leaves.extend(node.leaf_nodes())
+        return leaves
+
+    @property
+    def full_name(self):
+        return self.delimiter.join([p.name for p in self.parents if p.parents] + [self.name])
+
+    def __str__(self):
+        return str(self.name)
--- unit-tests/test_main.py
+++ unit-tests/test_main.py
@@ -1,6 +1,6 @@
 """ Unit tests for the `main` module. """
 
-from as_dataframe.main import as_dataframe, _flattened
+from as_dataframe.main import as_dataframe, _flattened, TreeNode, _flattened_2, _gathered
 from pandas import DataFrame
 from unittest import TestCase
 from copy import deepcopy
@@ -39,7 +39,8 @@ class TestAsDataframeFunction(TestCase):
             'Financial.Profit.After Tax': 80,
         })
 
-        self.assertTrue(expected_df.equals(as_dataframe(nested_dict)))
+        actual_df = as_dataframe(nested_dict)
+        self.assertTrue(expected_df.equals(actual_df))
 
     def test_with_list_of_nested_dicts_with_list_value(self):
 
@@ -71,8 +72,68 @@ class TestAsDataframeFunction(TestCase):
         expected_df = DataFrame({'a': [1], 'b': [2], 'c': ['foo']})
         self.assertTrue(expected_df.equals(as_dataframe(self.flat_dict_with_simple_values)))
 
+    def test_all_nulls_column_is_kept(self):
+        """ Test that a column with all NULLs is kept. """
+        dict_with_all_nones_for_a_key = {
+            'c': [
+                {'e': 1, 'f': {'g': 1, 'h': 1}},
+                {'e': 1, 'f': None}
+            ]
+        }
+        expected = DataFrame({
+            'c.e': [1, 1],
+            'c.f.g': [1, None],
+            'c.f.h': [1, None]
+        })
+        actual = as_dataframe(dict_with_all_nones_for_a_key)
+        print(actual)
+        self.assertTrue(expected.equals(actual))
 
-class TestFlattenedFunction(TestCase):
+#
+# class TestFlattenedFunction(TestCase):
+#     def test_with_3_levels_of_list_of_dicts(self):
+#
+#         nested = {
+#             'a': [
+#                 {
+#                     'b': [
+#                         {
+#                             'c': [
+#                                 {'x': 1, 'y': 2, 'z': 3},
+#                                 {'x': 4, 'y': 5, 'z': 6},
+#                                 {'x': 7, 'y': 8, 'z': 9}
+#                             ]
+#                         }
+#                     ]
+#                 }
+#             ]
+#         }
+#
+#         expected_flattened = {
+#             'a.b.c.x': [1, 4, 7],
+#             'a.b.c.y': [2, 5, 8],
+#             'a.b.c.z': [3, 6, 9]
+#         }
+#
+#         self.assertEqual(_flattened(nested), expected_flattened)
+
+
+class TestTreeNode(TestCase):
+    def test_leaf_nodes(self):
+        tree = TreeNode('root', None)
+
+        node_a = TreeNode('a')
+        node_aa = TreeNode('aa')
+        node_a.add_child(node_aa)
+
+        node_aa.add_child(TreeNode('aaa', data=3))
+
+        tree.add_child(node_a)
+
+        print({node.full_name: node.data for node in tree.leaf_nodes()})
+
+
+class TestFlattened2Function(TestCase):
     def test_with_3_levels_of_list_of_dicts(self):
 
         nested = {
@@ -97,5 +158,33 @@ class TestFlattenedFunction(TestCase):
             'a.b.c.z': [3, 6, 9]
         }
 
-        self.assertEqual(_flattened(nested), expected_flattened)
+        self.assertEqual(_flattened_2(nested), expected_flattened)
 
+    def test_with_simple_dict(self):
+        simple = {'a': 1, 'b': 2, 'c': 'foo'}
+
+        self.assertEqual(_flattened_2(simple), simple)
+
+    def test_with_list_of_nested_dicts(self):
+        list_of_nested_dicts = {
+            'a': [
+                {'c': 1, 'd': {'e': 2}},
+                {'c': 1, 'd': None}
+            ]
+        }
+
+        expected = {'a.c': [1, 1], 'a.d.e': [2, None]}
+
+        print(_flattened_2(list_of_nested_dicts))
+        self.assertEqual(_flattened_2(list_of_nested_dicts), expected)
+
+
+class TestGatheredFunction(TestCase):
+
+    def test_with_mixed_values(self):
+        mixed = [
+            {'c': 1, 'd.e': 2},
+            {'c': 1}
+        ]
+
+        print(_gathered(mixed))
